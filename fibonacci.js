a274332a78d76455bc68636f72dde11769ff8e80
tylersortijas <tylersortijas@gmail.com>
2023-03-31 13:20:18 -0700

    completed recursion functions

---
+++ fibonacci.js
@@ -0,0 +1,33 @@
+// Assignment 1 (Fibonacci Sequence):
+
+// Non-Recursive Version:
+
+function fibonacci(num) {
+  if (num < 0 || num === 0) {
+    return "OOPS";
+  }
+
+  let prevNum = 0;
+  let sum = 1;
+  for (let i = 0; i < num; i++) {
+    const temp = sum;
+    sum = prevNum + sum;
+    prevNum = temp;
+  }
+  return sum;
+}
+
+// Recursive Version:
+function recursiveFibonacci(num, sequence = [0, 1]) {
+  if (sequence.length >= num) {
+    return sequence.slice(0, num);
+  }
+
+  const newNumber = sequence.at(-1) + sequence.at(-2);
+  sequence.push(newNumber);
+  return recursiveFibonacci(num, sequence);
+}
+
+console.log(recursiveFibonacci(0));
+console.log(recursiveFibonacci(1));
+console.log(recursiveFibonacci(8));
